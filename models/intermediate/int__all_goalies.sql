18aad83e18a64ac27aa6e4bfe634773f41e92907
Benoit Cambournac <benoit@southshore.llc>
2024-07-18 18:34:42 +0000

    fully functional marts team rosters table - complete with all skaters and goalies for each team

---
+++ models/intermediate/int__all_goalies.sql
@@ -0,0 +1,28 @@
+-- models/intermediate/int__all_goalies.sql
+
+with all_goalies as (
+    select 
+        team_abv, 
+        goalies
+    from {{ ref("stg_nhl__team_rosters") }}
+)
+
+
+SELECT 
+    team_abv,
+    player.value: id::INT as player_id,
+    player.value: firstName.default::STRING as first_name,
+    player.value: lastName.default::STRING as last_name,
+    player.value: positionCode::STRING as position,
+    player.value: sweaterNumber::INT as number,
+    player.value: heightInInches::INT as height,
+    player.value: weightInPounds::INT as weight,
+    player.value: shootsCatches::STRING as shoots,
+    player.value: birthDate::STRING as birthDate,
+    player.value: birthCity.default::STRING as birth_city,
+    player.value: birthStateProvince.default::STRING as birth_state,
+    player.value: birthCountry::STRING as birth_country,
+    player.value: headshot::STRING as headshot_url
+FROM 
+    all_goalies,
+    LATERAL FLATTEN(input => goalies) player
