ce829acc3c1293910cc62557fcbf346beebcd305
Benoit Cambournac <benoit@southshore.llc>
2024-10-18 13:57:42 +0000

    updated statwise rankings

--- models/marts/current_statwise_rankings.sql
+++ models/marts/current_statwise_rankings.sql
@@ -1,9 +1,14 @@
 -- models/marts/current_statwise_rankings.sql
 -- updated team rankings of every team in every stat category
 
-{% set columns = ['goals_per_game_rank', 'shots_per_game_rank', 'p1_shots_per_game_rank', 'p2_shots_per_game_rank', 'p3_shots_per_game_rank', 
-'p1_goals_rank', 'p2_goals_rank', 'p3_goals_rank', 'ot_goals_rank', 'ot_shots_rank', 'pp_goals_rank', 'pp_attempts_rank', 'pp_pct_rank',
-'pim_per_game_rank', 'hits_per_game_rank', 'blocks_per_game_rank', 'takeaways_per_game_rank', 'giveaways_per_game_rank'] %}
+{% set columns = ['goals_per_game_rank', 'shots_per_game_rank', 'pp_goals_rank', 'pp_pct_rank',
+'pim_per_game_rank', 'hits_per_game_rank', 'blocks_per_game_rank'] %}
+
+/*
+, 'p1_shots_per_game_rank''p2_shots_per_game_rank', 'p3_shots_per_game_rank', 
+'p1_goals_rank', 'p2_goals_rank', 'p3_goals_rank', 'ot_goals_rank', 'ot_shots_rank', 'pp_attempts_rank'
+, 'takeaways_per_game_rank', 'giveaways_per_game_rank'
+*/
 
 with season_stats as (
     select *
@@ -14,22 +19,22 @@ ranked_stats as (
     select
         ROW_NUMBER() over(order by goals_per_game desc) as goals_per_game_rank,
         ROW_NUMBER() over(order by shots_per_game desc) as shots_per_game_rank,
-        ROW_NUMBER() over(order by p1_shots_per_game desc) as p1_shots_per_game_rank,
-        ROW_NUMBER() over(order by p2_shots_per_game desc) as p2_shots_per_game_rank,
-        ROW_NUMBER() over(order by p3_shots_per_game desc) as p3_shots_per_game_rank,
-        ROW_NUMBER() over(order by p1_goals desc) as p1_goals_rank,
-        ROW_NUMBER() over(order by p2_goals desc) as p2_goals_rank,
-        ROW_NUMBER() over(order by p3_goals desc) as p3_goals_rank,
-        ROW_NUMBER() over(order by ot_goals desc) as ot_goals_rank,
-        ROW_NUMBER() over(order by ot_shots desc) as ot_shots_rank,
+        -- ROW_NUMBER() over(order by p1_shots_per_game desc) as p1_shots_per_game_rank,
+        -- ROW_NUMBER() over(order by p2_shots_per_game desc) as p2_shots_per_game_rank,
+        -- ROW_NUMBER() over(order by p3_shots_per_game desc) as p3_shots_per_game_rank,
+        -- ROW_NUMBER() over(order by p1_goals desc) as p1_goals_rank,
+        -- ROW_NUMBER() over(order by p2_goals desc) as p2_goals_rank,
+        -- ROW_NUMBER() over(order by p3_goals desc) as p3_goals_rank,
+        -- ROW_NUMBER() over(order by ot_goals desc) as ot_goals_rank,
+        -- ROW_NUMBER() over(order by ot_shots desc) as ot_shots_rank,
         ROW_NUMBER() over(order by pp_goals desc) as pp_goals_rank,
-        ROW_NUMBER() over(order by pp_attempts desc) as pp_attempts_rank,
+        -- ROW_NUMBER() over(order by pp_attempts desc) as pp_attempts_rank,
         ROW_NUMBER() over(order by pp_pct desc) as pp_pct_rank,
         ROW_NUMBER() over(order by pim_per_game desc) as pim_per_game_rank,
         ROW_NUMBER() over(order by hits_per_game desc) as hits_per_game_rank,
         ROW_NUMBER() over(order by blocks_per_game desc) as blocks_per_game_rank,
-        ROW_NUMBER() over(order by takeaways_per_game desc) as takeaways_per_game_rank,
-        ROW_NUMBER() over(order by giveaways_per_game desc) as giveaways_per_game_rank,
+        -- ROW_NUMBER() over(order by takeaways_per_game desc) as takeaways_per_game_rank,
+        -- ROW_NUMBER() over(order by giveaways_per_game desc) as giveaways_per_game_rank,
         *
     from season_stats
 )
