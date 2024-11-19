9372f3aafdd324d27aaa0a1b6ca52a4f9ebab6a5
Benoit Cambournac <benoit@southshore.llc>
2024-11-19 16:04:03 +0000

    added play by play data, from staging to production

--- models/staging/stg_nhl__play_by_play.sql
+++ models/staging/stg_nhl__play_by_play.sql
@@ -4,8 +4,30 @@ with all_plays as (
 )
 
 select 
-    id, 
-    season, 
-    gamedate as date,
-    plays
-from all_plays
+    id::int as id, 
+    season::int as season, 
+    -- gamedate as date,
+    -- gametype::int as game_type,
+    -- awayteam:id::int as away_id,
+    -- awayteam:abbrev::string as away_abv,
+    -- hometeam:id::int as home_id,
+    -- hometeam:abbrev::string as home_abv,
+    -- plays.value as full_play,
+    plays.value:eventId::int as event_id,
+    plays.value:homeTeamDefendingSide::string as home_side,
+    plays.value:periodDescriptor.number::int as period,
+    plays.value:periodDescriptor.periodType::string as period_type,
+    plays.value:situationCode::int as situation_code,
+    plays.value:sortOrder::int as sort_order,
+    plays.value:timeInPeriod::string as time_in_period,
+    split_part(time_in_period, ':', 1)::int * 60 + split_part(time_in_period, ':', -1)::int as elapsed_seconds,
+    plays.value:timeRemaining::string as time_remaining,
+    plays.value:typeCode::int as type_code,
+    plays.value:typeDescKey::string as description,
+    plays.value:details.xCoord as x_pos,
+    plays.value:details.yCoord as y_pos,
+    plays.value:details.zoneCode::string as zone_code,
+    plays.value:details as full_details
+    -- count(*)
+from all_plays,
+lateral flatten (input => all_plays.plays) plays
